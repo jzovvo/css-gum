fc1dddf0e0573f3fe882efb4be4e6a899fc5737b
jzovvo <jzovvo@gmail.com>
2025-08-17 01:26:45 +0800

    chore: validate 重寫

--- src/core.ts
+++ src/core.ts
@@ -5,45 +5,81 @@ import { checkDesignDraftScalingParams, checkPercentParams, checkViewportParams,
 
 export const vw = (pixelData: Pixel, designDraftData: DesignDraft) => {
   const result = checkViewportParams(pixelData, designDraftData)
-  return result ? cssPxToVw(result[1])(result[0]) : ''
+  if (result.data) {
+    return cssPxToVw(result.data[1])(result.data[0])
+  }
+  console.error(new Error('[ vw error ]\n' + result.error))
+  return ''
 }
 
 export const lvw = (pixel: Pixel, designDraftData: DesignDraft) => {
   const result = checkViewportParams(pixel, designDraftData)
-  return result ? cssPxToVwClamp(result[1])(result[0]) : ''
+  if (result.data) {
+    return cssPxToVwClamp(result.data[1])(result.data[0])
+  }
+  console.error(new Error('[ lvw error ]\n' + result.error))
+  return ''
 }
 
 export const vh = (pixel: Pixel, designDraftData: DesignDraft) => {
   const result = checkViewportParams(pixel, designDraftData)
-  return result ? cssPxToVh(result[1])(result[0]) : ''
+  if (result.data) {
+    return cssPxToVh(result.data[1])(result.data[0])
+  }
+  console.error(new Error('[ vh error ]\n' + result.error))
+  return ''
 }
 
 export const lvh = (pixel: Pixel, designDraftData: DesignDraft) => {
   const result = checkViewportParams(pixel, designDraftData)
-  return result ? cssPxToVhClamp(result[1])(result[0]) : ''
+  if (result.data) {
+    return cssPxToVhClamp(result.data[1])(result.data[0])
+  }
+  console.error(new Error('[ lvh error ]\n' + result.error))
+  return ''
 }
 
 export const percent = (child: Percent, parent: Percent) => {
   const result = checkPercentParams(child, parent)
-  return result ? cssPercent(result[1])(result[0]) : ''
+  if (result.data) {
+    return cssPercent(result.data[1])(result.data[0])
+  }
+  console.error(new Error('[ percent error ]\n' + result.error))
+  return ''
 }
 
 export const ddws = (pixel: Pixel, designDraftData: DesignDraft, percent: Percent = 0.5) => {
   const result = checkDesignDraftScalingParams(pixel, designDraftData, percent)
-  return result ? cssDesignDraftVwScaling(result[1])(result[2])(result[0]) : ''
+  if (result.data) {
+    return cssDesignDraftVwScaling(result.data[1])(result.data[2])(result.data[0])
+  }
+  console.error(new Error('[ ddws error ]\n' + result.error))
+  return ''
 }
 
 export const ddhs = (pixel: Pixel, designDraftData: DesignDraft, percent: Percent = 0.5) => {
   const result = checkDesignDraftScalingParams(pixel, designDraftData, percent)
-  return result ? cssDesignDraftVhScaling(result[1])(result[2])(result[0]) : ''
+  if (result.data) {
+    return cssDesignDraftVhScaling(result.data[1])(result.data[2])(result.data[0])
+  }
+  console.error(new Error('[ ddhs error ]\n' + result.error))
+  return ''
 }
 
 export const em = (child: Percent, parent: Percent) => {
   const result = checkPercentParams(child, parent)
-  return result ? cssEm(result[0], result[1]) : ''
+  if (result.data) {
+    return cssEm(result.data[0], result.data[1])
+  }
+  console.error(new Error('[ em error ]\n' + result.error))
+  return ''
 }
 
 export const lh = (child: Percent, parent: Percent) => {
   const result = checkPercentParams(child, parent)
-  return result ? cssLh(result[0], result[1]) : ''
+  if (result.data) {
+    return cssLh(result.data[0], result.data[1])
+  }
+  console.error(new Error('[ lh error ]\n' + result.error))
+  return ''
 }
--- src/validate.ts
+++ src/validate.ts
@@ -1,58 +1,61 @@
-import { designDraftSchema, percentSchema, pixelSchema, type DesignDraft, type Percent, type Pixel } from "./types"
-import { fromError } from 'zod-validation-error'
+import { designDraftSchema, percentSchema, pixelSchema } from "./types"
 
 
-export const checkViewportParams = (pixel: unknown, designDraftData: unknown): [Pixel, DesignDraft] | null => {
+export const checkViewportParams = (pixel: unknown, designDraftData: unknown) => {
   const pixelDataResult = pixelSchema.safeParse(pixel)
-
-  if (!pixelDataResult.success) {
-    console.error(new Error(`pixel 接收到 ${pixel} designDraftData 接收到 ${designDraftData},` + fromError(pixelDataResult.error).toString()))
-    return null
-  }
-
   const designDraftDataResult = designDraftSchema.safeParse(designDraftData)
-  if (!designDraftDataResult.success) {
-    console.error(new Error(`pixel 接收到 ${pixel} designDraftData 接收到 ${designDraftData},` + fromError(designDraftDataResult.error).toString()))
-    return null
+
+  if (pixelDataResult.success && designDraftDataResult.success) {
+    return {
+      data: [pixelDataResult.data, designDraftDataResult.data],
+      error: null
+    }
   }
 
-  return [pixelDataResult.data, designDraftDataResult.data]
+  return {
+    data: null,
+    error:
+      `pixel expected number, received ${pixel}\n` +
+      `designDraftData expected number, received ${designDraftData}`
+  }
 }
 
-export const checkPercentParams = (child: unknown, parent: unknown): [Percent, Percent] | null => {
+export const checkPercentParams = (child: unknown, parent: unknown) => {
   const childResult = percentSchema.safeParse(child)
-  if (!childResult.success) {
-    console.error(new Error(fromError(childResult.error).toString()))
-    return null
-  }
-
   const parentResult = percentSchema.safeParse(parent)
-  if (!parentResult.success) {
-    console.error(new Error(fromError(parentResult.error).toString()))
-    return null
+
+  if (childResult.success && parentResult.success) {
+    return {
+      data: [childResult.data, parentResult.data],
+      error: null
+    }
   }
 
-  return [childResult.data, parentResult.data]
+  return {
+    data: null,
+    error:
+      `child expected number, received ${child}\n` +
+      `parent expected number, received ${parent}`
+  }
 }
 
-export const checkDesignDraftScalingParams = (pixel: unknown, designDraftData: unknown, percent: unknown): [Pixel, DesignDraft, Percent] | null => {
+export const checkDesignDraftScalingParams = (pixel: unknown, designDraftData: unknown, percent: unknown) => {
   const pixelDataResult = pixelSchema.safeParse(pixel)
-  if (!pixelDataResult.success) {
-    console.error(new Error(fromError(pixelDataResult.error).toString()))
-    return null
-  }
-
   const designDraftDataResult = designDraftSchema.safeParse(designDraftData)
-  if (!designDraftDataResult.success) {
-    console.error(new Error(fromError(designDraftDataResult.error).toString()))
-    return null
-  }
-
   const percentResult = percentSchema.safeParse(percent)
-  if (!percentResult.success) {
-    console.error(new Error(fromError(percentResult.error).toString()))
-    return null
+
+  if (pixelDataResult.success && designDraftDataResult.success && percentResult.success) {
+    return {
+      data: [pixelDataResult.data, designDraftDataResult.data, percentResult.data],
+      error: null
+    }
   }
 
-  return [pixelDataResult.data, designDraftDataResult.data, percentResult.data]
+  return {
+    data: null,
+    error:
+      `pixel expected number, received ${pixel}\n` +
+      `designDraftData expected number, received ${designDraftData}\n` +
+      `percent expected number, received ${percent}`
+  }
 }
