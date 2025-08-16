d032af502195de87acdd646e830c987a85287d88
jzovvo <jzovvo@gmail.com>
2025-08-16 22:04:06 +0800

    chore: 補上 tsconfig & dts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,16 +2,19 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig((options) => (
   {
-  entry: [
-    'src/index.ts'
-  ],
-  minify: !options.watch,
-  format: [
-    'esm',
-    'cjs',
-  ],
-  target: [
-    'es6'
-  ],
-  treeshake: true,
+    clean: true,
+    entry: [
+      'src/index.ts'
+    ],
+    minify: !options.watch,
+    format: [
+      'esm',
+      'cjs',
+    ],
+    target: [
+      'es6'
+    ],
+    treeshake: true,
+    dts: true,
+    sourcemap: true,
 }))
