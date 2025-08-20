c5ae281f0bcdb5dc258903c3851bf313b79bb502
jzovvo <jzovvo@gmail.com>
2025-08-20 23:44:59 +0800

    style: 目錄結構修改

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import * as Core from './core'
-import * as Gen from './gen'
-import * as Utils from './utils'
-import * as Snippet from './snippets'
+import * as Core from './modules/core'
+import * as Gen from './modules/gen'
+import * as Utils from './modules/utils'
+import * as Snippet from './modules/snippets'
 
 export {Core, Gen, Utils, Snippet}
